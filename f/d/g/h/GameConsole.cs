a70f6b36f1383ebe94a3e36fa938ca0fff68dfeb
Joshua12432 <115190964+Joshua12432@users.noreply.github.com>
2023-01-07 18:13:56 -0800

    Add files via upload

---
+++ f/d/g/h/GameConsole.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace JSharpProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        Console.Title = "Game ConsoleV2";
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.WindowHeight = 40;
+            Console.WindowWidth = 40;
+            
+
+            Console.WriteLine("Program:Welcome To the Game C# LauncherV2");
+            Console.WriteLine("Whats your fav coding language?");
+            Console.ReadLine();
+            Console.WriteLine("Cool! My fav is python.");
+        }
+    }
+}
